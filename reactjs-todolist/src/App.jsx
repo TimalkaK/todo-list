adb1682ff7b5fb9ad9699b8b048fa0ab35861a21
TimalkaK <timalkamk@gmail.com>
2024-09-12 16:20:37 +0100

    feat: Implemented stateful variables to update and add to todo list

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,15 +1,22 @@
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
+import { useState } from "react";
 
 function App() {
-  let todos = [
+  const [todos, setTodos] = useState([
     "Go to the gym",
     "Eat more fruits and veg",
     "Pick up the kids from school",
-  ];
+  ]);
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo];
+    setTodos(newTodoList);
+  }
+
   return (
     <>
-      <TodoInput todos={todos} />
+      <TodoInput handleAddTodos={handleAddTodos} />
       <TodoList todos={todos} />
     </>
   );
